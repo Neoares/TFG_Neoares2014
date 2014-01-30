3c368b285b791c3646482a2f9858e10eb42eb08d
Neoares <neoares23@gmail.com>
2014-01-30 23:45:21 +0100

    request/response y POST implementado

---
+++ test/main/utils.js
@@ -0,0 +1,12 @@
+function formValues(data){
+    var splits = data.split('&');
+    var hash = [];
+    console.log(splits.length);
+    for (i = 0; i < splits.length; i++)
+    {
+        var iSplit = splits[i].split('=');
+        hash[iSplit[0]] = iSplit[1];
+    }
+    return hash;
+}
+exports.formValues = formValues;
