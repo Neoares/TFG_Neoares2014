f7703645b060ac6f806d9c6fba9bd18179976a9d
Neoares <neoares23@gmail.com>
2014-02-05 00:47:01 +0100

    conexió i interacció amb la base de dades.
    de moment no es fa servir mongoDB

--- server/main/server.js
+++ server/main/server.js
@@ -1,19 +1,32 @@
 //var url = require("url");
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port: 8888});
+var fs = require('fs');
+var index = fs.readFileSync('../web/index.html');
+
+var format = require('util').format;
+
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+  user: 'root',
+  password: '',
+  host: '127.0.0.1',
+  port: '3306',
+});
 
 function iniciar(route, handle) {
-	
+	client.query('USE testing');
+	client.query("INSERT INTO Usuario VALUES (1, 'user1', 'pass1', 'username1', 1)");
 	wss.on('connection', function(ws) {
 		ws.on('message', function(message) {
 			console.log('received: %s', message);
 		});
-		ws.send('te has conectado a mí');
+		ws.send('enviando web');
+		ws.send(index);
 		//ws.terminate();
 	});
   console.log("Servidor Iniciado.");
 }
 
 exports.iniciar = iniciar;
-
-
